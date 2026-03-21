358ae01d9470f063aa67e2638df10b2880825575
Alessandro Mazzeo <alessandromazzeo@Mac.lan>
2026-03-21 08:48:34 +0100

    Auto-save: aggiornamento automatico

--- api/quote.js
+++ api/quote.js
@@ -1,3 +1,5 @@
+import { fetchQuoteSummary } from './_lib/yahoo.js';
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
